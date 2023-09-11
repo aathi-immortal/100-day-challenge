19666d009f8f3adb1309c6f96b3096bb04be5bbf
aathi github <aathisnr123@gmail.com>
2023-09-12 00:59:08 +0530

    StockBuypro

--- day-1/Array/ReverseMathLongestSum.java
+++ day-1/Array/ReverseMathLongestSum.java
@@ -1,76 +1,57 @@
-import java.util.HashMap;
-import java.util.Map;
 
 public class ReverseMathLongestSum {
-    public static int setMax(int size,int temSize)
-    {
+    public static int setMax(int size, int temSize) {
         return Math.max(temSize, size);
-                
-        
+
     }
-    public static int longestSubarrayWithSumK(int []a, long k) {
+
+    public static int longestSubarrayWithSumK(int[] a, long k) {
         int i = 0;
         int j = 0;
         int sum = a[0];
         int size = 0;
-        while(j < a.length - 1)
-        {
-            if(sum == k)
-            {
+        while (j < a.length - 1) {
+            if (sum == k) {
                 size = setMax(size, j - i + 1);
-                sum += a[++j];        
-            }
-            else if(sum > k)
-            {
-                if(i == j)
-                {
+                sum += a[++j];
+            } else if (sum > k) {
+                if (i == j) {
                     sum += a[++j];
                 }
                 sum -= a[i++];
-            }
-            else
-            {
+            } else {
                 sum += a[++j];
             }
-            
+
         }
-        if(sum == k)
-        {
-            size = setMax(size, j- i + 1);
+        if (sum == k) {
+            size = setMax(size, j - i + 1);
         }
         return size;
 
-
-
-
-
-
-
-
-        //        int i = 0;
+        // int i = 0;
         // int sum = 0;
         // int size = 0;
         // Map<Integer, Integer> map = new HashMap<>();
         // while (i < a.length) {
-        //     sum += a[i];
-        //     if ((long) sum == k) {
-        //         size = Math.max(size, i + 1);
-        //     }
-            
-        //     else if (map.containsKey((int) (sum - k))) {
-        //             size = Math.max(size, i - map.get((int) (sum - k)));
-        //             // System.out.println((sum - k) + " " + i + ":" + map.get((int) (sum - k)));
-        //         }
-        
-        //     if(!map.containsKey(sum))
-        //     {
-        //         map.put(sum, i);
-        //     }
-        //     i++;
+        // sum += a[i];
+        // if ((long) sum == k) {
+        // size = Math.max(size, i + 1);
+        // }
+
+        // else if (map.containsKey((int) (sum - k))) {
+        // size = Math.max(size, i - map.get((int) (sum - k)));
+        // // System.out.println((sum - k) + " " + i + ":" + map.get((int) (sum - k)));
+        // }
+
+        // if(!map.containsKey(sum))
+        // {
+        // map.put(sum, i);
+        // }
+        // i++;
         // }
         // return size;
 
     }
- 
-    
-} 
+
+}
---
+++ day-1/Array/StockBuySell.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class StockBuySell {
+    static Stack<Integer> stack;
+
+    static void loadTheStack(int[] arr) {
+
+        stack.push(arr[arr.length - 1]);
+
+        for (int i = arr.length - 2; i > -1; i--) {
+            if (stack.peek() <= arr[i]) {
+                stack.push(arr[i]);
+            }
+
+        }
+    }
+
+    public static int bestTimeToBuyAndSellStock(int[] prices) {
+        stack = new Stack<>();
+        // load the stack based on max prize from last to start
+        loadTheStack(prices);
+        int max = 0;
+        // main logic
+        for (int i = 0; i < prices.length; i++) {
+            int profit = stack.peek() - prices[i];
+            if (stack.peek() == prices[i]) {
+                stack.pop();
+            }
+            if (profit > max) {
+                max = profit;
+            }
+        }
+        return max;
+    }
+}
