b995bf8928c7bfa3a4e2ca93fb844aaaf97207bd
aathi github <aathisnr123@gmail.com>
2023-10-03 20:52:58 +0530

    day-13 removing bugs

--- day13/FruitsBasket.java
+++ day13/FruitsBasket.java
@@ -7,38 +7,51 @@ import io.ArrayIo;
 // 10 8 25 3 23 3 3 28 2 24 7 2 22 12 18 16 1 16 6 1 6 20 10 3 2 0 25 5 14 
 
 public class FruitsBasket {
-    public static int findMaxFruits(int[] arr, int n) {
-        if (arr.length == 1) {
-            return 1;
-        }
-        HashMap<Integer, Integer> map = new HashMap<>();
 
+    public static int findMaxFruits(int[] arr, int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int start = 0;
         int end = 0;
         int max = 0;
-        int count = 0;
         while (end < n) {
-            if (map.size() < 2) {
-                map.put(arr[end], 1);
-            } else if (!map.containsKey(arr[end])) {
-                map = new HashMap<>();
-                map.put(arr[end], 1);
-                count = 1;
+            // window stringing
+            if (!map.containsKey(arr[end]) && map.size() == 2) {
+                // window will string untill we have 2 unique value
+                while (map.size() == 2) {
+                    map.put(arr[start], map.get(arr[start]) - 1);
+                    if (map.get(arr[start]) == 0) {
+                        map.remove(arr[start]);
+                    }
+                    start++;
+
+                    // if(map.containsKey(map.get(arr[start])))
+                    // {
+
+                    // }
+                }
             }
-            count++;
+            // window increasing
+            if (map.containsKey(arr[end])) {
+                map.put(arr[end], map.get(arr[end]) + 1);
+            } else {
+                map.put(arr[end], 1);
+            }
             end++;
 
-            // max check
-            if (max < count) {
-                max = count;
+            // max value
+            if (max < (end - start)) {
+                max = (end - start);
             }
         }
         return max;
-
     }
 
     public static void main(String[] args) {
-        int[] arr = ArrayIo.buildArray();
-        int value = findMaxFruits(arr, arr.length);
-        System.out.println(value);
+        while (true) {
+            int[] arr = ArrayIo.buildArray();
+            int value = findMaxFruits(arr, arr.length);
+            System.out.println(value);
+        }
+
     }
 }
