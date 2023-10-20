22c69a84c440eface63a2380e3109629c8e85779
aathi github <aathisnr123@gmail.com>
2023-10-20 22:12:41 +0530

    removing consecutive duplicates

---
+++ day23/DuplicateInString.java
@@ -0,0 +1,27 @@
+public class DuplicateInString {
+
+    public static String removeConsecutiveDuplicates(String s) {
+
+        String ans = "";
+        int start = 0;
+        int end = 1;
+        while (end < s.length()) {
+
+            Character startCharacter = s.charAt(start);
+            Character nexCharacter = s.charAt(end);
+            if (startCharacter.compareTo(nexCharacter) == 0) {
+                end++;
+            } else {
+                ans += startCharacter;
+                start = end;
+
+            }
+
+        }
+
+        ans += s.charAt(start);
+
+        return ans;
+    }
+
+}
