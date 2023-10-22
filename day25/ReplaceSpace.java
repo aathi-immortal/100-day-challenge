f1cb169135cf9b8833aa6c00b42cc0fe9fa2e7c2
aathi github <aathisnr123@gmail.com>
2023-10-22 17:04:40 +0530

    with constant space

--- day25/ReplaceSpace.java
+++ day25/ReplaceSpace.java
@@ -6,25 +6,41 @@ public class ReplaceSpace {
         Scanner scan = new Scanner(System.in);
         System.out.println("enter the string:");
         String str = scan.nextLine();
-        replaceSpaces(new StringBuilder(str));
+        String ans = replaceSpaces(new StringBuilder(str)).toString();
+        System.out.println(ans);
 
     }
 
     public static StringBuilder replaceSpaces(StringBuilder str) {
 
+        // int i = 0;
+        // StringBuilder ansStr = new StringBuilder();
+        // while (i < str.length()) {
+
+        // String character = str.charAt(i) + "";
+        // if (character.compareTo(" ") == 0) {
+
+        // ansStr.append("@40");
+        // } else {
+        // ansStr.append(character);
+        // }
+        // i++;
+        // }
+        // return ansStr;
         int i = 0;
-        StringBuilder ansStr = new StringBuilder();
+        System.out.println(str.length());
         while (i < str.length()) {
 
             String character = str.charAt(i) + "";
             if (character.compareTo(" ") == 0) {
+                str.deleteCharAt(i);
+                str.insert(i, "@40");
+             
 
-                ansStr.append("@40");
             } else {
-                ansStr.append(character);
+                i++;
             }
-            i++;
         }
-        return ansStr;
+        return str;
     }
 }
