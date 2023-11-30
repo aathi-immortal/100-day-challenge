80fab6d1711d8665623687fcdc0b429083773a59
aathi github <aathisnr123@gmail.com>
2023-11-30 21:14:07 +0530

    platform using greety approch

--- day36/TrainPlatForm.java
+++ day36/TrainPlatForm.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.*;
 
 class Train implements Comparable<Train> {
     int arrivalTime;
@@ -13,25 +14,28 @@ class Train implements Comparable<Train> {
 
     @Override
     public int compareTo(Train o) {
-        return (this.arrivalTime > o.arrivalTime) ? -1 : 1;
+        return (this.arrivalTime > o.arrivalTime) ? 1 : -1;
     }
 
 }
 
-public class TrainPlatForm {
+public class Solution {
     public static int calculateMinPatforms(int at[], int dt[], int n) {
         // sort the at abd dt array based on the at array
-        ArrayList<Train> trains = new ArrayList<>();
-        loadArray(trains, at, dt);
-        Collections.sort(trains);
+        // ArrayList<Train> trains = new ArrayList<>();
+        // loadArray(trains, at, dt);
+        // Collections.sort(trains);
+        // display(trains);
+        Arrays.sort(at);
+        Arrays.sort(dt);
         int arrivalIndex = 1;
         int departureIndex = 0;
         int platformNumber = 1;
         int maxPlatform = 1;
 
         while (arrivalIndex < n) {
-            int arrivalTime = trains.get(arrivalIndex).arrivalTime;
-            int departureTime = trains.get(departureIndex).departureTime;
+            int arrivalTime = at[arrivalIndex];
+            int departureTime = dt[departureIndex];
             // if arrival time is less than departure time
             if (arrivalTime <= departureTime) {
                 platformNumber++;
@@ -51,6 +55,13 @@ public class TrainPlatForm {
     private static void loadArray(ArrayList<Train> trains, int[] at, int[] dt) {
         for (int i = 0; i < dt.length; i++) {
             trains.add(new Train(at[i], dt[i]));
+        }
+    }
+
+    static void display(ArrayList<Train> datas) {
+        for (Train data : datas) {
+            System.out.println(data.arrivalTime + " " + data.departureTime);
+
         }
     }
 }
