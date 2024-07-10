a70cd0c82cb9f89abe3a0ac2059a16342d1d7624
aathi github <aathisnr123@gmail.com>
2024-07-10 08:32:06 +0530

    zoho set-2 alternative sorting

---
+++ zoho-set2/AlternativeSorting.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AlternativeSorting {
+    ArrayList<Long> alternateSort(long arr[], int N) {
+
+        // sort the array
+        // create dummy array
+        // insert the data in alternatively
+        ArrayList<Long> dummy = new ArrayList<>();
+        Arrays.sort(arr);
+        int lastIndex = N - 1;
+        int firstIndex = 0;
+        while (firstIndex < lastIndex) {
+            dummy.add(arr[lastIndex]);
+            dummy.add(arr[firstIndex]);
+        }
+        if (firstIndex == lastIndex) {
+            dummy.add(arr[firstIndex]);
+        }
+        return dummy;
+    }
+}
